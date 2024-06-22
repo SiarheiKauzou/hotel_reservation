59e5664fb4cbb1028b7584b0e0b1fdcda71d5155
siarhei_kauzou <sergei@clearbluedesign.com>
2024-06-22 21:52:36 +0300

    add database layer

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,37 @@
 package api
 
 import (
+	"context"
+
+	"github.com/SiarheiKauzou/hotel_reservation/db"
 	"github.com/SiarheiKauzou/hotel_reservation/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandlerGetUsers(c *fiber.Ctx) error {
-	u := types.User{
-		FirstName: "James",
-		LastName: "At the watercooler",
-	}
-	return c.JSON(u)
+type UserHandler struct {
+	userStore db.UserStore
 }
 
-func HandlerGetUser(c *fiber.Ctx) error {
-	return c.JSON("James")
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{userStore: userStore}
+}
+
+func (h *UserHandler) HandlerGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandlerGetUsers(c *fiber.Ctx) error {
+	u := types.User{
+		FirstName: "James",
+		LastName:  "At the watercooler",
+	}
+	return c.JSON(u)
 }
